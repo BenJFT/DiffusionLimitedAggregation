fa062bf38dfb5638dcb922c3efa713566d615156
Benjft <benjfthompson@gmail.com>
2017-01-03 18:15:16 +0000

    functions for transposing matricies and finding the regression coefficients of a set of data points

--- util/util.go
+++ util/util.go
@@ -2,8 +2,10 @@ package util
 
 import (
 	"os"
+	"math"
 	"bufio"
 	"strings"
+	"github.com/gonum/plot/plotter"
 )
 
 func StringToArgs(str string) (args []string) {
@@ -26,4 +28,54 @@ func ReadStrOrEmpty() string {
 	} else {
 		return ""
 	}
+}
+
+func Transpose(data [][]float64) [][]float64 {
+	out := make([][]float64, len(data[0]))
+	for i := range out {
+		out[i] = make([]float64, len(data))
+	}
+
+	for i, row := range data {
+		for j, y := range row {
+			out[j][i] = y
+		}
+	}
+	return out
+}
+
+
+func LeastSquares(xys plotter.XYs) (a, b, ea, eb float64) {
+	var (
+		S2, SSxx, SSyy, SSxy, meanX, meanY float64
+		n float64 = float64(len(xys))
+	)
+
+	// calculating sum of squares
+	for _, xy := range xys {
+		SSxx += xy.X * xy.X
+		SSyy += xy.Y * xy.Y
+		SSxy += xy.X * xy.Y
+		meanX += xy.X
+		meanY += xy.Y
+	}
+
+	meanX /= n
+	meanY /= n
+
+	SSxx -= meanX * meanX * n
+	SSyy -= meanY * meanY * n
+	SSxy -= meanX * meanY * n
+
+	// use sum of squares to find a & b for y = a + bx
+	b = SSxy / SSxx
+	a = meanY - b*meanX
+
+	// find the standard error on each of these coefficients
+	S2 = (SSyy - b * SSxy) / (n - 2)
+
+	ea = math.Sqrt( S2 * meanX*meanX/(SSxx * n) )
+	eb = math.Sqrt( S2/SSxx )
+
+	return a, b, ea, eb
 }
