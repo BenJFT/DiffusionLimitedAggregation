e751f353f01a07fd69218d92bfc93807b81fc433
Benjft <benjfthompson@gmail.com>
2017-01-04 15:08:44 +0000

    Comments and cleanup

--- processing/processing.go
+++ processing/processing.go
@@ -1,25 +1,29 @@
+/*
+Author: Benedict Thompson
+*/
+
 package processing
 
 import (
-	"os"
+	"bufio"
+	"encoding/gob"
 	"fmt"
 	"math"
 	"math/rand"
-	"bufio"
-	"encoding/gob"
+	"os"
 
-	"github.com/Benjft/DiffusionLimitedAggregation/aggregation"
+	"github.com/Benjft/DiffusionLimitedAggregation/processing/aggregation"
 	"github.com/Benjft/DiffusionLimitedAggregation/processing/analysis"
 	"github.com/Benjft/DiffusionLimitedAggregation/util"
 	"github.com/Benjft/DiffusionLimitedAggregation/util/drawing/svg"
 	"github.com/Benjft/DiffusionLimitedAggregation/util/drawing/xyz"
 
-	"github.com/skratchdot/open-golang/open"
 	"github.com/gonum/plot"
 	"github.com/gonum/plot/plotter"
-	"github.com/gonum/plot/vg"
 	"github.com/gonum/plot/plotutil"
+	"github.com/gonum/plot/vg"
 
+	"github.com/skratchdot/open-golang/open"
 )
 
 func init() {
@@ -30,132 +34,121 @@ func init() {
 	gob.Register(RunState{})
 }
 
-
+// Structure used to store all info about a previous run, including it's parameters and results
 type RunState struct {
 	NPoints, NDimension, NRuns, Seed int64
-	Sticking float64
-	Points [][]aggregation.Point
+	Sticking                         float64
+	Points                           [][]aggregation.Point
 }
-var (
-	loadedRun = RunState{}
-)
 
-func run(nPoints, seed, nDimension int64, sticking float64, chanel chan []aggregation.Point) {
-	chanel <- aggregation.RunNew(nPoints, seed, nDimension, sticking)
-}
+// The most recent successful run or loaded run from save. All opps are done on the contents of this
+var loadedRun = RunState{}
+
 func Run(nPoints, nRuns, seed, nDimension int64, sticking float64) {
 	var (
-		ch0, ch1, ch2, ch3 chan []aggregation.Point
-		points [][]aggregation.Point = make([][]aggregation.Point, nRuns)
-		out []aggregation.Point
+		channel chan []aggregation.Point = make(chan []aggregation.Point)
+		points  [][]aggregation.Point    = make([][]aggregation.Point, nRuns)
 	)
 
 	rand.Seed(seed)
+	for i := int64(0); i < nRuns; i++ {
+		// sets each run to go concurrently, sending their results over 'channel'. This improves the run time
+		// when there are a large number of runs
+		go func(nPoints, seed, nDimension int64, sticking float64) {
+			channel <- aggregation.RunNew(nPoints, seed, nDimension, sticking)
+		}(nPoints, rand.Int63(), nDimension, sticking)
+	}
 
-	for i := int64(0); i < nRuns; {
-
-		if ch0 == nil {
-			ch0 = make(chan []aggregation.Point)
-			go run(nPoints, rand.Int63(), nDimension, sticking, ch0)
-		} else if ch1 == nil && nRuns > 1 {
-			ch1 = make(chan []aggregation.Point)
-			go run(nPoints, rand.Int63(), nDimension, sticking, ch1)
-		} else if ch2 == nil && nRuns > 2 {
-			ch2 = make(chan []aggregation.Point)
-			go run(nPoints, rand.Int63(), nDimension, sticking, ch2)
-		} else  if ch3 == nil && nRuns > 3 {
-			ch3 = make(chan []aggregation.Point)
-			go run(nPoints, rand.Int63(), nDimension, sticking, ch3)
+	// waits for each output and adds the value to the 'points' array
+	for i := int64(0); i < nRuns; i++ {
+		out := <-channel
+		if out == nil {
+			fmt.Println("Run Failed!")
+			return
 		} else {
-			select {
-			case out = <-ch0:
-				points[i] = out
-				if nRuns > 4 && i < nRuns-1 {
-					go run(nPoints, rand.Int63(), nDimension, sticking, ch0)
-				}
-			case out = <-ch1:
-				points[i] = out
-				if nRuns > 4 && i < nRuns-1 {
-					go run(nPoints, rand.Int63(), nDimension, sticking, ch1)
-				}
-			case out = <-ch2:
-				points[i] = out
-				if nRuns > 4 && i < nRuns-1 {
-					go run(nPoints, rand.Int63(), nDimension, sticking, ch2)
-				}
-			case out = <-ch3:
-				points[i] = out
-				if nRuns > 4 && i < nRuns-1 {
-					go run(nPoints, rand.Int63(), nDimension, sticking, ch3)
-				}
-			}
-
-			if out == nil {
-				fmt.Println("Run failed!")
-				return
-			} else {
-				i++
-			}
+			points[i] = out
 		}
 	}
 
-	loadedRun = RunState {
-		NPoints: nPoints,
+	// none of the runs failed update the loaded run
+	loadedRun = RunState{
+		NPoints:    nPoints,
 		NDimension: nDimension,
-		NRuns: nRuns,
-		Seed: seed,
-		Sticking: sticking,
-		Points: points,
+		NRuns:      nRuns,
+		Seed:       seed,
+		Sticking:   sticking,
+		Points:     points,
 	}
 }
 
-func draw3D(state []aggregation.Point, title string, display bool) {
-	var name string = fmt.Sprintf("out\\plot\\%s.%s", title, "xyz")
+// Saves the 3D aggregate as an 'xyz' file. These can be opened using many 3D graphics editors, I know that a blender 3D
+// extension exists for importing these. 'xyz' was the chosen file type as i don't have to deal with geometry this way
+func draw3D(state []aggregation.Point, title string) {
+	var name string = fmt.Sprintf("out\\plot\\%s.xyz", title)
+
+	// attempts to create a new file with the provided name
 	file, err := os.Create(name)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
+	// ensures the file is closed when the function returns or program stops
 	defer file.Close()
+
+	//handles writing the data to the opened file
 	writer := bufio.NewWriter(file)
+	// gets the plaintext formatting of the state as an xyz file
 	str := xyz.Format(state)
+	// writes the bytes representing the string to the file
 	_, err = writer.Write([]byte(str))
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	if display {
-		err = open.Run(name)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
+
+	// If nothing went wrong, open the file using the system default
+	err = open.Run(name)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
 	}
 }
-func draw2D(state []aggregation.Point, title string, display bool) {
+
+// Saves the 2D aggregate as an 'svg' file. This can be opened in most web browsers or vector graphics editors.
+func draw2D(state []aggregation.Point, title string) {
 	name := fmt.Sprintf("out\\plot\\%s.svg", title)
+
+	// attempts to create a new file with the provided name
 	file, err := os.Create(name)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
+	// ensures the file is closed when the function returns or program stops
 	defer file.Close()
+
+	//handles writing the data to the opened file
 	writer := bufio.NewWriter(file)
+	// gets the plaintext formatting of the state as an xyz file
 	str := svg.DrawAggregate(state)
+	// writes the bytes representing the string to the file
 	_, err = writer.Write([]byte(str))
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	if display {
-		err = open.Run(name)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
+
+	// If nothing went wrong, open the file using the system default
+	err = open.Run(name)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
 	}
 }
-func Draw(title string, display bool) {
+
+// Called to render the latest run (given that the run is in only 2 or 3 dimensions)
+func Draw(title string) {
+	// Create a default formatted title using the run parameters if no title was provided
 	if len(title) == 0 {
 		title = fmt.Sprintf("aggregate-n%d-seed%d-dims%d-stick%f",
 			loadedRun.NPoints,
@@ -164,36 +157,45 @@ func Draw(title string, display bool) {
 			loadedRun.Sticking)
 	}
 
+	// draw using the appropreate function for the number of dimensions
 	if n := loadedRun.NDimension; n == 2 {
 		for run, state := range loadedRun.Points {
 			runtitle := fmt.Sprintf("%s-run%d", title, run)
-			go draw2D(state, runtitle, display)
+			go draw2D(state, runtitle)
 		}
 	} else if n == 3 {
 		for run, state := range loadedRun.Points {
 			runtitle := fmt.Sprintf("%s-run%d", title, run)
-			go draw3D(state, runtitle, display)
+			go draw3D(state, runtitle)
 		}
 	} else {
 		fmt.Println("Can only draw 2D and 3D lattices")
 	}
 }
 
+// Saves #loadedRun using gob formatting so that it can bea easily loaded at a later time
 func Save(title string) {
+	// Create a default formatted title using the run parameters if no title was provided
 	if title == "" {
-		title = fmt.Sprintf("save-n%d-seed%d-dims%d-stick%f-runs%d", loadedRun.NPoints, loadedRun.Seed,
-			loadedRun.NDimension, loadedRun.Sticking, loadedRun.NRuns)
+		title = fmt.Sprintf("save-n%d-seed%d-dims%d-stick%f-runs%d",
+			loadedRun.NPoints,
+			loadedRun.Seed,
+			loadedRun.NDimension,
+			loadedRun.Sticking,
+			loadedRun.NRuns)
 	}
 	path := fmt.Sprintf("out\\saves\\%s.save", title)
 
+	// Create the file at the given location
 	file, err := os.Create(path)
 	if err != nil {
 		fmt.Println(err.Error())
 
 	}
-
+	// Ensures the file is closed when the function quits
 	defer file.Close()
 
+	// write the state to a gob in the given file
 	encoder := gob.NewEncoder(file)
 	err = encoder.Encode(loadedRun)
 	if err != nil {
@@ -201,17 +203,20 @@ func Save(title string) {
 	}
 }
 
+// Loads the named run into loadedRun
 func Load(title string) {
 	path := fmt.Sprintf("out\\saves\\%s.save", title)
 
+	// attempt to open the file fr reading
 	file, err := os.Open(path)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-
+	// Ensures the file is closed when the function quits
 	defer file.Close()
 
+	// read the gob in the file into a new Run struct
 	decoder := gob.NewDecoder(file)
 	var tmpRun RunState
 	err = decoder.Decode(&tmpRun)
@@ -220,22 +225,22 @@ func Load(title string) {
 		return
 	}
 
+	// If nothing went wrong set the make this the active Run
 	loadedRun = tmpRun
 }
 
-func radii(run []aggregation.Point, chanel chan []analysis.Ball) {
-	chanel <- analysis.ApproxBounding(run)
-}
-func Radii() {
-	var channels []chan []analysis.Ball = make([]chan []analysis.Ball, loadedRun.NRuns)
-	for i, run := range loadedRun.Points {
-		var channel chan []analysis.Ball = make(chan []analysis.Ball)
-		go radii(run, channel)
-		channels[i] = channel
+// uses the approximate radius of the aggregate to plot log(N)/log(R) and find the fractal dimensions
+func Radii(title string) {
+	// open a channel and wait to receive the radius calculations for each run in the current loaded state
+	var channel chan []analysis.Ball = make(chan []analysis.Ball)
+	for _, run := range loadedRun.Points {
+		go func(run []aggregation.Point) {
+			channel <- analysis.ApproxBounding(run)
+		}(run)
 	}
 
 	radii := make([][]float64, loadedRun.NRuns)
-	for i, channel := range channels {
+	for i := range loadedRun.Points {
 		radii[i] = make([]float64, loadedRun.NPoints)
 		runBalls := <-channel
 		for j, ball := range runBalls {
@@ -243,48 +248,79 @@ func Radii() {
 		}
 	}
 
-	//radius, stdErr := util.MeanAndErr(radii)
-
+	// flip the arrays so that each row contains the radius for the same number of particles (makes plotting
+	// scatters and calculating error easier)
 	radii = util.Transpose(radii)
 
 	pts := make([]plotter.XYer, len(radii))
 	allXY := plotter.XYs{}
+
+	// make arrays of points marking X=log(R) Y=log(N)
 	for i, r := range radii {
 		xys := make(plotter.XYs, len(r))
-		N := float64(i+1)
+		N := float64(i + 1)
 		for j, y := range r {
-			xys[j].X = math.Log10(y+.5)
+			xys[j].X = math.Log10(y + .5)
 			xys[j].Y = math.Log10(N)
 		}
 		pts[i] = xys
 
-		// Ignore the first few as growth will be non typical
+		// Ignore the first few as growth may be non typical
 		if i > len(radii)/50 {
 			allXY = append(allXY, xys...)
 		}
 	}
 
-	plt , err := plot.New()
+	// create a new plot of the data
+	plt, err := plot.New()
 	if err != nil {
-		panic(err)
+		fmt.Println(err.Error())
+		return
 	}
 
+	// plot the average after each point is added and find the error with 95% confidence.
 	mean95, err := plotutil.NewErrorPoints(plotutil.MeanAndConf95, pts...)
 	if err != nil {
-		panic(err)
+		fmt.Println(err.Error())
+		return
+	}
+	// add these to the plot
+	err = plotutil.AddScatters(plt, mean95)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	err = plotutil.AddXErrorBars(plt, mean95)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
 	}
 
+	// calculate the regression coefficients and their respective errors
 	a, b, _, eb := util.LeastSquares(allXY)
+	// Print the approximation of the fractal dimensions from the regression
 	fmt.Printf("D = %.3f \u00B1 %.3f\n", b, eb)
+	// add and label the Least Squares fit
 	label := fmt.Sprintf("y = %.3f + %.3fx", a, b)
-	fit := plotter.NewFunction(func (x float64) float64 { return a + b*x })
-
-	plotutil.AddScatters(plt, mean95)
-	plotutil.AddXErrorBars(plt, mean95)
+	fit := plotter.NewFunction(func(x float64) float64 { return a + b*x })
 	plt.Add(fit)
 	plt.Legend.Add(label, fit)
 
-	name := "out\\plot\\TEST.svg"
+	// Save and show the plot
+	if title == "" {
+		title = fmt.Sprintf("radii-n%d-seed%d-dims%d-stick%f-runs%d",
+			loadedRun.NPoints,
+			loadedRun.Seed,
+			loadedRun.NDimension,
+			loadedRun.Sticking,
+			loadedRun.NRuns)
+	}
+
+	name := fmt.Sprintf("out\\plot\\%s.svg", title)
 	plt.Save(15*vg.Inch, 10*vg.Inch, name)
-	open.Run(name)
+	err = open.Run(name)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 }
